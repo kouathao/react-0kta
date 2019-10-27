b97f7e6b919bba22d259ded1818fb4a8ee9c092b
Koua Thao <kouathao09@gmail.com>
2019-10-27 17:39:04 -0500

    finished app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// Okta
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
 
 // import compnents
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import Staff from "./components/pages/Staff";
+import Login from "./components/auth/Login";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/staff" exact={true} component={Staff} />
-        </div>
-      </div>
-    </Router>
-  );
+function onAuthRequired({ history }) {
+  history.push("/login");
+}
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security
+          issuer="https://dev-496891.okta.com/oauth2/default"
+          client_id="0oa1ogltofEmaZihm357"
+          redirect_uri={window.location.origin + "/implicit/callback"}
+          onAuthRequired={onAuthRequired}
+        >
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => <Login baseUrl="https://dev-496891.okta.com" />}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+            </div>
+          </div>
+        </Security>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/auth/SignInWidget.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import OktaSignIn from "@okta/okta-signin-widget";
+
+class SignInWidget extends Component {
+  componentDidMount() {
+    const el = ReactDOM.findDOMNode(this);
+    this.widget = new OktaSignIn({
+      baseUrl: this.props.baseUrl
+    });
+    this.widget.renderEl({ el }, this.props.onSuccess, this.props.onError);
+  }
+
+  componentWillUnmount() {
+    this.widget.remove();
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default SignInWidget;
